7bae354a96b3ef80798a545b188cd05b47130692
Soya <soya@mit.edu>
2017-11-24 03:51:57 -0500

    Convert since field into int for easy calc

--- src/java/simpledb/test.java
+++ src/java/simpledb/test.java
@@ -8,11 +8,23 @@ import org.json.simple.JSONObject;
 import org.json.simple.parser.JSONParser;
 import org.json.simple.parser.ParseException;
 import java.io.PrintWriter;
+import java.util.Date;
+import java.text.SimpleDateFormat;
+import java.text.*;
 public class test {
 
     public static void main(String[] argv) {
+        try {
+            
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+
+                    
+
         JSONParser parser = new JSONParser();
         try {   
+            
 
             JSONArray a = (JSONArray) parser.parse(new FileReader("g1.json"));
             PrintWriter writer = new PrintWriter("tt.txt", "UTF-8");
@@ -23,15 +35,34 @@ public class test {
             
                 Long id = (Long) person.get("id");
                 String since = (String) person.get("since");
+                since = since.split(" GMT")[0];
                 Long tabIndex = (Long) person.get("tabIndex");
                 Long time = (Long) person.get("time");
                 String title = (String) person.get("title");
                 Long windowID = (Long) person.get("windowID");
+
+                Long sinceNumber = 0L;
+
+                // Parse since field
+                try {
+                    // This object can interpret strings representing dates in the format MM/dd/yyyy
+                    DateFormat df = new SimpleDateFormat("EEE MMM dd yyyy hh:mm:ss"); 
                 
-                writer.println(id + ", " + since +  ", " + tabIndex + ", " + time + ", " + title + ", " + windowID);
+                            DateFormat df2 = new SimpleDateFormat("yyyyMMddhhmmss"); 
+                            
+                           // Convert from String to Date
+                           Date startDate = df.parse(since);
+                           String startDateString2 = df2.format(startDate);
+                           
+                           System.out.println("Date, with the default formatting: " + startDateString2);
+                           sinceNumber = Long.parseLong(startDateString2);
+                } catch (Exception e) {
+                    e.printStackTrace();
+                }
+                
+                writer.println(id + ", " + sinceNumber +  ", " + tabIndex + ", " + time + ", " + title + ", " + windowID);
                 
                 // System.out.println(name);
-
                 
             }
 
